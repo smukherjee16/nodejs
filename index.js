42c9dab99a1933bd8f8c3b4440cab1cd8d86fa33
smukherjee16 <tosayari@yahoo.com>
2019-08-27 10:17:27 -0400

    project update1

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.listen(port, () => {
 
 //load some default employees. Can set it to employee array also. 
 const employees = [
-    {id: 1, firstName: 'Mark', lastName: 'Anthony', role: 'CEO', hireDate: '2017-12-10', 
+    {id: 1, firstName: 'Mark', lastName: 'Anthony', role: 'VP', hireDate: '2017-12-10', 
         favoriteMessage1: 'messaege1', favoriteMessage2: 'message2'},
     {id: 2, firstName: 'Dave', lastName: 'Barton', role: 'MANAGER', hireDate: '2017-10-10', 
         favoriteMessage1: 'messaege11', favoriteMessage2: 'message22'}
